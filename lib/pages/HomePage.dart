c8fb8822ffbf2fdd23a6115d7fe322f573d9e267
MaskyS <maskys@hackers.mu>
2018-04-05 17:21:14 +0400

    Move the SystemChrome methods to main.dart and also lock orientation of device.

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import '../UI/StudentoDrawer.dart';
 import '../UI/HomePageBody.dart';
@@ -9,11 +8,7 @@ import '../UI/HomePageBody.dart';
 class HomePage extends StatelessWidget{
 
   static String routeName = "home_page";
-  Widget build(BuildContext context){
-    // Hide the status bar. We don't want students to be distracted by
-    // notifications.
-    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.values[1]]);
-
+  Widget build(BuildContext context){;
     return new Scaffold(
       drawer: new StudentoDrawer(usedInHomePage : true),
       // Since this page has a SliverAppBar, we cannot use the appBar property
