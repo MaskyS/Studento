a7b7ef92d2adadd7b8db699fa053eec2c28880a0
MaskyS <maskys@hackers.mu>
2018-04-24 11:31:20 +0400

    Refactor RandomQuotesContainer.dart so it doesn't throw up 'method () <= called on null' error.

--- lib/UI/RandomQuoteContainer.dart
+++ lib/UI/RandomQuoteContainer.dart
@@ -3,70 +3,71 @@ import 'dart:math';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
-
 class RandomQuoteContainer extends StatefulWidget {
   @override
   RandomQuoteContainerState createState() => new RandomQuoteContainerState();
 }
 
 class RandomQuoteContainerState extends State<RandomQuoteContainer> {
-
   List quotesList;
-  int quotesListLength;
 
-  getQuotes() async {
-    var fileData = await rootBundle.loadString('assets/json/quotes.json');
-    this.setState(() {
-      quotesList = JSON.decode(fileData);
-      quotesListLength = quotesList.length;
+  void _getQuotes() {
+    // Get the quotes from our json file and put it into quotesList.
+    rootBundle.loadString('assets/json/quotes.json').then((String fileData){
+      setState(() {
+        quotesList = json.decode(fileData);
+      });
     });
   }
 
   @override
   void initState() {
-    this.getQuotes();
+    _getQuotes();
   }
 
   @override
   Widget build(BuildContext context) {
-  final _random = new Random();
-  // get a random number which will be used to choose a quote from the list.
-  int randomQuoteIndex = _random.nextInt(quotesListLength);
-
-  final String quoteString = quotesList[randomQuoteIndex]["Quote String"];
-  final String quoteAuthorNameString = quotesList[randomQuoteIndex]["Quote Author"];
-
-  //The text Widget for the quote.
-  final Container quoteText  = new Container(
-    alignment: Alignment.center,
-    padding: new EdgeInsets.symmetric(vertical: 20.0),
-    child: new Text(quoteString,
-      style: new TextStyle(
-        fontWeight: FontWeight.w400,
-        fontSize: 18.0,
-        color: Colors.white,
-      ),
-      textAlign: TextAlign.center,
-    ),
-  );
-
-  // The text Widget for the author name.
-  Container  quoteAuthorText = new Container(
-    child: new Text(('- $quoteAuthorNameString'),
-      textAlign: TextAlign.end,
-      style: new TextStyle(
-        fontWeight: FontWeight.w400,
-        color: Colors.white,
-      ),
-    ),
-    alignment: FractionalOffset.bottomRight,
-    padding: new EdgeInsets.only(top: 5.0),
-  );
-
-  return new ListView(
-    children: <Widget>[
-      quoteText,
-      quoteAuthorText,
-    ],);
+    return new ListView(
+      children: // While the quotes are being loaded, display a progress indicator.
+        (quotesList!=null) ?
+          _buildRandomQuoteWidget()
+          : <Widget>[new CircularProgressIndicator()],
+    );
   }
+
+  List<Widget> _buildRandomQuoteWidget(){
+    // get a random number that will be used to get a quote from the list.
+    final random = new Random();
+    int randomQuoteIndex = random.nextInt(quotesList.length);
+    // get the random quote and the corresponding author.
+    final String quoteString = quotesList[randomQuoteIndex]["Quote String"];
+    final String quoteAuthorNameString = quotesList[randomQuoteIndex]["Quote Author"];
+
+    Container quoteTextContainer = new Container(
+      alignment: Alignment.center,
+      padding: new EdgeInsets.symmetric(vertical: 20.0),
+      child: new Text(quoteString,
+        style: new TextStyle(
+          fontSize: 18.0,
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+
+    Container authorNameContainer = new Container(
+      alignment: FractionalOffset.bottomRight,
+      padding: new EdgeInsets.only(top: 5.0),
+      child: new Text(('- $quoteAuthorNameString'),
+        textAlign: TextAlign.end,
+        style: new TextStyle(
+          fontWeight: FontWeight.w400,
+          color: Colors.white,
+        ),
+      ),
+    );
+
+    return <Widget>[quoteTextContainer, authorNameContainer,];
+  }
+
 }
