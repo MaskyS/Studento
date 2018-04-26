e5d848605c3002f856b68bc748521d01e414f1ad
MaskyS <maskys@hackers.mu>
2018-04-26 23:42:56 +0400

    Add Easter egg.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,3 @@
-// This is a basic Flutter widget test.
-// To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
-// provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
-// find child widgets in the widget tree, read text, and verify that the values of widget properties
-// are correct.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:studentapp/main.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new Studento());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+/// April Fools! This app is developed by a high school student with not much
+/// time on his hands because of ... tests. School ones ;p So please help him
+/// out on https://github.com/MaskyS/studento/pulls to create tests :D
