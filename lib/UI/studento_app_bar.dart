a24f8f5e2ecdc63a1e473e6ca5425079aa746a8d
MaskyS <maskys@hackers.mu>
2018-08-04 13:50:37 +0400

    Change theme icon default color to black87

--- lib/UI/studento_app_bar.dart
+++ lib/UI/studento_app_bar.dart
@@ -6,15 +6,16 @@ class StudentoAppBar extends AppBar {
   StudentoAppBar(
     {Key key,
     String title: "Studento",
-    TextStyle titleStyle : const TextStyle(color: Colors.white),
+    TextStyle titleStyle : const TextStyle(color: Colors.black87),
     PreferredSizeWidget bottom,
+    IconThemeData iconTheme: const IconThemeData(color: Colors.black87),
     List<Widget> actions}
   ): super(
     key: key,
-    title: Text(title, style: titleStyle),
+    title: Text(title, style: titleStyle, textScaleFactor: 1.2,),
     actions: actions,
     centerTitle: true,
-    backgroundColor: Colors.deepPurpleAccent,
+    backgroundColor: Colors.white,
     bottom: bottom
   );
 }
