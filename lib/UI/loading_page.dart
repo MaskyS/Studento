df58c76967022f9ed2c4f51dd7931cfe23dc2b4c
MaskyS <maskys@hackers.mu>
2019-04-22 19:47:17 +0400

    Clean up repo

--- lib/UI/loading_page.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget loadingPage() {
-  return Center(child: CircularProgressIndicator());
-}
--- test/widget_test.dart
+++
@@ -1,3 +0,0 @@
-/// April Fools! This app is developed by a high school student with not much
-/// time on his hands because of ... tests. School ones ;p So please help him
-/// out on https://github.com/MaskyS/studento/pulls to create tests :D
